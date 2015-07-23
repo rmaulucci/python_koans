d890cdd5adc4a176e6ac57f1bb2e2d0ec08d2ba8
rmaulucci <rmaulucci11@gmail.com>
2015-07-23 17:37:06 -0400

    Updates
    End of day 7/23/15

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,16 +18,28 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    test_1 = set([a,b,c])
-    if len(test_1) == 1:
-        return 'equilateral'
-    elif len(test_1) == 2:
-        return 'isosceles'
-    else:
-        return 'scalene'
+    try:
+        pretest(a,b,c)
+        test_1 = set([a,b,c])
+        if len(test_1) == 1:
+            return 'equilateral'
+        elif len(test_1) == 2:
+            return 'isosceles'
+        else:
+            return 'scalene'
+    except StandardError:
+        raise TriangleError
     
+def pretest(a, b, c):
+    pretest_1 = [a,b,c]
+    pretest_1.sort()
+    cond_1 = (pretest_1[0] + pretest_1[1]) > pretest_1[2]
+    cond_2 = (sum(x > 0 for x in pretest_1) == len(pretest_1)) 
 
+    if cond_1 and cond_2:
+        return True
+    else:
+        raise StandardError
 
-# Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
     pass
