875d3f8a9ff525e24116863f645745f4db577185
rmaulucci <rmaulucci11@gmail.com>
2015-07-23 14:17:00 -0400

    Revert "Initial commit"
    This reverts commit 3996979a78fd7556fdfc776f258353ad0821e55d.

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,14 +18,8 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    test_1 = set([a,b,c])
-    if len(test_1) == 1:
-        return 'equilateral'
-    elif len(test_1) == 2:
-        return 'isosceles'
-    else:
-        return 'scalene'
-    
+    # DELETE 'PASS' AND WRITE THIS CODE
+    pass
 
 
 # Error class used in part 2.  No need to change this code.
